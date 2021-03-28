442679c8cee971e17db7af0d1f49713ed0021aef
mohammdelzanaty <mohamed.elzanaty3@vodafone.com>
2021-03-28 10:37:47 +0200

    01-componets-part-2

--- mz-shop/src/App.js
+++ mz-shop/src/App.js
@@ -1,17 +1,24 @@
-const Product = () => {
+const Product = (props) => {
     return React.createElement("div", {}, [
-        React.createElement("h1", {}, "Mens Cotton Jacket"),
-        React.createElement("h2", {}, "Great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions,"),
-        React.createElement("h2", {}, "55.99$"),
+        React.createElement("h1", {}, props.title),
+        React.createElement("h2", {}, props.description),
+        React.createElement("h2", {}, props.price),
     ]);
 };
 
 const App = () => {
     return React.createElement("div", {}, [
         React.createElement("h1", {}, "MZ Shop"),
-        React.createElement(Product),
-        React.createElement(Product),
-        React.createElement(Product),
+        React.createElement(Product, {
+            title: "Samsung SE450",
+            description: "21.5-inch desktop business monitor offers superior ergonomics and eco-friendly features – constructed with 30%",
+            price: "89.99$",
+        }),
+        React.createElement(Product, {
+            title: "Mac Book Pro",
+            description: "our perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+            price: "700$",
+        }),
     ]);
 };
 
