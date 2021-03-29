39c564868e4c0e2fc1dc5c609b7481e94c72a9ea
mohammdelzanaty <mohamed.elzanaty3@vodafone.com>
2021-03-29 13:37:15 +0200

    05-state-life-cycle-method

--- mz-shop/src/App.js
+++ mz-shop/src/App.js
@@ -1,26 +1,36 @@
 import React, { Component } from 'react'
+import { getAllProduct } from './api/ProductAPI'
 import Product from './Product'
 
 class App extends Component {
+  state = {
+    products: []
+  }
+
+  componentDidMount() {
+    // get products
+    getAllProduct().then(products => {
+      if (Array.isArray(products)) {
+        this.setState({ products })
+      }
+    })
+  }
+
   render() {
     return (
       <div>
         <div className="product-list">
-          <Product
-            title="Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops"
-            description="Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday"
-            price="89.99$"
-            category="men clothing"
-            image="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
-          />
-
-          <Product
-            title="Mens Casual Premium Slim Fit T-Shirts"
-            description="Slim-fitting style, contrast raglan long sleeve, three-button henley placket,"
-            price="89.99$"
-            category="men clothing"
-            image="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"
-          />
+          {this.state.products &&
+            this.state.products.map(product => (
+              <Product
+                key={product.id}
+                title={product.title}
+                image={product.image}
+                price={product.price}
+                category={product.category}
+                description={product.description}
+              />
+            ))}
         </div>
       </div>
     )
