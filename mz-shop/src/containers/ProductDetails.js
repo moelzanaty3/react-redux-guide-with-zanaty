ba46061c9cd24866923cee46fe2880dc896f5754
mohammdelzanaty <mohamed.elzanaty3@vodafone.com>
2021-04-05 17:39:12 +0200

    11 add details product page

--- mz-shop/src/containers/ProductDetails.js
+++ mz-shop/src/containers/ProductDetails.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react'
+import { getProductById } from '../api/ProductAPI'
+import { Link } from 'react-router-dom'
 
 class ProductDetails extends Component {
+  state = {
+    product: {}
+  }
+
   componentDidMount() {
     // get product
+    const productId = this.props.match.params.id
+    getProductById(productId).then(product => {
+      this.setState({ product })
+    })
   }
 
   render() {
-    console.log(this.props)
-    return <div>Product Details</div>
+    return (
+      <div>
+        <Link className="close-product-details" to="/">
+          Close
+        </Link>
+        <pre>
+          <code>{JSON.stringify(this.state.product, null, 2)}</code>
+        </pre>
+      </div>
+    )
   }
 }
 
