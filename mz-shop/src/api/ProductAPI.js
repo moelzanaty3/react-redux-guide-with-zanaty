17b41b0e5f2f038ef7426af78495544d7fe87c14
mohammdelzanaty <mohamed.elzanaty3@vodafone.com>
2021-04-05 17:40:29 +0200

    clean files

--- mz-shop/src/api/ProductAPI.js
+++ mz-shop/src/api/ProductAPI.js
@@ -1,34 +1,27 @@
 const BASE_URL = 'https://fakestoreapi.com'
 
-export const getProductById = (productId) =>
+export const getProductById = productId =>
   fetch(`${BASE_URL}/products/${productId}`)
-    .then((res) => res.json())
-    .then((data) => data)
-    .catch((error) => {
+    .then(res => res.json())
+    .then(data => data)
+    .catch(error => {
       console.log(error)
     })
 
-export const deleteProduct = (productId) =>
+export const deleteProduct = productId =>
   fetch(`${BASE_URL}/products/${productId}`, {
-    method: "DELETE",
+    method: 'DELETE'
   })
-    .then((res) => res.json())
-    .then((data) => data)
-    .catch((error) => {
+    .then(res => res.json())
+    .then(data => data)
+    .catch(error => {
       console.log(error)
     })
 
 export const getAllProduct = () =>
   fetch(`${BASE_URL}/products`)
-    .then((res) => res.json())
-    .then((data) => data)
-    .catch((error) => {
+    .then(res => res.json())
+    .then(data => data)
+    .catch(error => {
       console.log(error)
     })
-
-export const searchProduct = async (query) => {
-  const products = await getAllProduct()
-  return products.filter(
-    (product) => product.title.toLowerCase().indexOf(query.toLowerCase()) !== -1
-  )
-}
