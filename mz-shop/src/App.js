02045ab8b4c6fa78f3dd105a375b5f4934bde8c8
mohammdelzanaty <mohamed.elzanaty3@vodafone.com>
2021-04-05 15:36:30 +0200

    06 add search input and sorting products by price

--- mz-shop/src/App.js
+++ mz-shop/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import sortBy from 'sort-by'
 import { getAllProduct } from './api/ProductAPI'
 import Product from './Product'
 
 class App extends Component {
   state = {
-    products: []
+    products: [],
+    query: ''
   }
 
   componentDidMount() {
@@ -16,21 +18,62 @@ class App extends Component {
     })
   }
 
+  handleSearchProduct = query => {
+    this.setState(() => ({
+      query: query.trim()
+    }))
+  }
+
+  clearQuery = () => {
+    this.handleSearchProduct('')
+  }
+
   render() {
+    const { query, products } = this.state
+    const showingProducts =
+      query === ''
+        ? products
+        : products.filter(p => p.title.toLowerCase().includes(query.toLowerCase()))
+
     return (
       <div>
-        <div className="product-list">
-          {this.state.products &&
-            this.state.products.map(product => (
-              <Product
-                key={product.id}
-                title={product.title}
-                image={product.image}
-                price={product.price}
-                category={product.category}
-                description={product.description}
-              />
-            ))}
+        <div className="list-products">
+          <div className="list-products-top">
+            <input
+              className="search-products"
+              type="text"
+              placeholder="Search Products"
+              value={query}
+              onChange={event => {
+                this.handleSearchProduct(event.target.value)
+              }}
+            />
+          </div>
+
+          {showingProducts.length !== products.length && (
+            <div className="showing-products">
+              <span>
+                Now showing {showingProducts.length} of {products.length}
+              </span>
+              <button onClick={this.clearQuery}>Show all</button>
+            </div>
+          )}
+
+          <ol className="product-list">
+            {showingProducts &&
+              showingProducts
+                .sort(sortBy('price'))
+                .map(product => (
+                  <Product
+                    key={product.id}
+                    title={product.title}
+                    image={product.image}
+                    price={product.price}
+                    category={product.category}
+                    description={product.description}
+                  />
+                ))}
+          </ol>
         </div>
       </div>
     )
