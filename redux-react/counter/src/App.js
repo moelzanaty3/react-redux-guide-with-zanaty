6afdbe6cd659ff4fc6485da9d7bdc6f5337ed28d
mohammdelzanaty <mohamed.elzanaty3@vodafone.com>
2021-04-21 14:35:58 +0200

    Redux-React: counter -> Create the store

--- redux-react/counter/src/App.js
+++ redux-react/counter/src/App.js
@@ -1,16 +1,53 @@
-import './App.css';
+import { createStore } from 'redux'
+import { connect, Provider } from 'react-redux'
+import './App.css'
+
+const initialState = {
+  count: 0,
+}
+
+const INCREMENT = 'INCREMENT'
+const DECREMENT = 'DECREMENT'
+const RESET = 'RESET'
+
+const incrementValue = () => ({
+  type: INCREMENT,
+})
+
+const decrementValue = () => ({
+  type: DECREMENT,
+})
+
+const resetValue = () => ({
+  type: RESET,
+})
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return { count: state.count + 1 }
+    case DECREMENT:
+      return { count: state.count - 1 }
+    case RESET:
+      return { count: 0 }
+    default:
+      return state
+  }
+}
+
+const store = createStore(reducer)
 
 function App() {
   return (
-      <main className="Counter">
-        <p className="count">0</p>
-        <section className="controls">
-          <button>Increment</button>
-          <button>Decrement</button>
-          <button>Reset</button>
-        </section>
-      </main>
-  );
+    <main className="Counter">
+      <p className="count">0</p>
+      <section className="controls">
+        <button>Increment</button>
+        <button>Decrement</button>
+        <button>Reset</button>
+      </section>
+    </main>
+  )
 }
 
-export default App;
+export default App
