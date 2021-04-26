c0373fb161f8b81f3848d869522c31efad440f67
mohammdelzanaty <mohamed.elzanaty3@vodafone.com>
2021-04-26 12:24:40 +0200

    Redux-React: todos -> add base structure without redux

---
+++ redux-react/todos/src/components/Todo.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import cx from 'classnames'
+
+const Todo = ({ todo }) => (
+  <li className="todo-item">
+    {todo && todo.completed ? '👌' : '👋'}{' '}
+    <span
+      className={cx(
+        'todo-item__text',
+        todo && todo.completed && 'todo-item__text--completed',
+      )}
+    >
+      {todo.content}
+    </span>
+  </li>
+)
+
+export default Todo
---
+++ redux-react/todos/src/components/index.js
@@ -0,0 +1,4 @@
+import Todo from './Todo'
+import AddTodo from './AddTodo'
+
+export { Todo, AddTodo }
