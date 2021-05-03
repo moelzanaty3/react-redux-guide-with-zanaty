d101b5622a3eec1d29e181e93b2693e55adcf49b
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2021-05-03 19:48:10 +0200

    add remove authed user redux

--- would-you-rather-game/src/actions/authed-user.action.js
+++ would-you-rather-game/src/actions/authed-user.action.js
@@ -1,8 +1,14 @@
-import { SET_AUTHED_USER } from '../constants'
+import { REMOVE_AUTHED_USER, SET_AUTHED_USER } from '../constants'
 
 export function setAuthedUser(userId) {
   return {
     type: SET_AUTHED_USER,
     payload: { userId },
+  }
+}
+
+export function removeAuthedUser(userId) {
+  return {
+    type: REMOVE_AUTHED_USER,
   }
 }
--- would-you-rather-game/src/reducers/authed-user.reducer.js
+++ would-you-rather-game/src/reducers/authed-user.reducer.js
@@ -1,10 +1,13 @@
-import { SET_AUTHED_USER } from '../constants'
+import { REMOVE_AUTHED_USER, SET_AUTHED_USER } from '../constants'
 
 export default function authedUserReducer(authedUser = null, action) {
   switch (action.type) {
     case SET_AUTHED_USER: {
       const { userId } = action.payload
       return userId
+    }
+    case REMOVE_AUTHED_USER: {
+      return null
     }
     default:
       return authedUser
